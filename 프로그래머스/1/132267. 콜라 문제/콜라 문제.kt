f192c781c17a073dcfc7e92a6d6a81c3c37ecb62
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-19 14:50:04 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
@@ -1,12 +1,5 @@
 class Solution {
     fun solution(a: Int, b: Int, n: Int): Int {
-        var answer: Int = 0
-        var coke = n
-        
-        while(coke >= a) {
-            answer += coke/a*b
-            coke = coke/a*b +coke%a
-        }
-        return answer
+        return (if (n > b) n - b else 0) / (a - b) * b
     }
 }
