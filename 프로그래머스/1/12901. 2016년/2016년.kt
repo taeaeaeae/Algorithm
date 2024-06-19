49e5231188f8f9bd49e0e17329902389bc0bc5a7
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-19 17:22:06 +0900

    [level 1] Title: 2016년, Time: 18.41 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.kt
+++ 프로그래머스/1/12901. 2016년/2016년.kt
@@ -2,13 +2,13 @@ class Solution {
     fun solution(a: Int, b: Int): String {
         var answer = ""
         val w = listOf("SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT")
-        var mon = (a)*61
-        
-        if(a == 4 ) mon +=5
-        else if(a==7|| a ==10 || a==2) mon +=4
-        else if(a == 1  || a == 12) mon +=6
-        else if(a%3==0) mon
-        else mon +=2
+        val mon = when(a) {
+            4 -> a*61+5 
+            7,10,2 -> a*61 +4
+            1 ,12 -> a*61 + 6
+            3, 6, 9 ->  a*61
+            else -> a*61+2
+        }
         return w[(mon+b)%7]
     }
 }
