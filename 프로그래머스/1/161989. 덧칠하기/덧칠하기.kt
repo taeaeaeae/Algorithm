d063573a9892dd8da248e85fc8d0b938738d652f
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-20 10:13:39 +0900

    [level 1] Title: 덧칠하기, Time: 1.46 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
@@ -1,14 +1,17 @@
 class Solution {
     fun solution(n: Int, m: Int, section: IntArray): Int {
-        var answer: Int = 1
+        var answer: Int = 0
         var i = 0
-        var now = section[0]
-        for(i in section.indices){
-            if(section[i] > now + m -1) { 
-                now = section[i]
+        var j = 1
+        while(j<section.size) {
+            if(section[i]+m-1>section[j]) {
                 answer++
-            }
+            } else i++
+            j++
+            
+            println("${answer}???? ${i}!!!!${j}")
         }
+        
         return answer
     }
 }
